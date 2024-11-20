569af4b4c87333a6df362a88f7a30d3729f76424
jwatson <jkwatson@gmail.com>
2024-11-20 11:04:56 -0800

    hide the knowledge base switch for now

--- ui/src/pages/RagChatTab/FooterComponents/RagChatQueryInput.tsx
+++ ui/src/pages/RagChatTab/FooterComponents/RagChatQueryInput.tsx
@@ -135,6 +135,7 @@ const RagChatQueryInput = () => {
                   checkedChildren={<DatabaseFilled />}
                   value={!queryConfiguration.exclude_knowledge_base}
                   onChange={handleExcludeKnowledgeBase}
+                  style={{ display: "none" }} // note: disabled for now, until UX is ready
                 />
               </Tooltip>
             }
