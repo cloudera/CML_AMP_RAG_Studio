f4a2596c92001a8d48875a7236c38955f08e6a52
Elijah Williams <ewilliams@cloudera.com>
2024-11-21 15:34:29 -0700

    pdm fastapi

--- scripts/startup_app.sh
+++ scripts/startup_app.sh
@@ -62,7 +62,7 @@ qdrant/qdrant &
 # start Python backend
 cd llm-service
 #source venv/bin/activate
-fastapi run --host 127.0.0.1 --port 8081 &
+pdm run fastapi run --host 127.0.0.1 --port 8081 &
 
 # start up the jarva
 cd ..
