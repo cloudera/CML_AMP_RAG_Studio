de3ccb85585c4063e37a1e3f31e6c7358ff3a5e3
jwatson <jkwatson@gmail.com>
2025-01-08 11:33:34 -0800

    remove some extraneous logging

--- llm-service/app/services/data_sources_metadata_api.py
+++ llm-service/app/services/data_sources_metadata_api.py
@@ -61,9 +61,6 @@ class RagDataSource:
 
 
 BACKEND_BASE_URL = os.getenv("API_URL", "http://localhost:8080")
-print("*******************************")
-print("API_URL: ", BACKEND_BASE_URL)
-print("*******************************")
 url_template = BACKEND_BASE_URL + "/api/v1/rag/dataSources/{}"
 
 
