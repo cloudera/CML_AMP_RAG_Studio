4fa84d945266c7e37f3fce172d7a3f830ecb5df5
jwatson <jkwatson@gmail.com>
2025-01-23 13:29:00 -0800

    fix the perf testing code for the new structures

--- llm-service/performance_testing/performance_testing.py
+++ llm-service/performance_testing/performance_testing.py
@@ -35,36 +35,6 @@
 #  BUSINESS ADVANTAGE OR UNAVAILABILITY, OR LOSS OR CORRUPTION OF
 #  DATA.
 #
-
-#
-#  CLOUDERA APPLIED MACHINE LEARNING PROTOTYPE (AMP)
-#  (C) Cloudera, Inc. 2024
-#  All rights reserved.
-#
-#  Applicable Open Source License: Apache 2.0
-#
-#
-#  This code is provided to you pursuant a written agreement with
-#  (i) Cloudera, Inc. or (ii) a third-party authorized to distribute
-#  this code. If you do not have a written agreement with Cloudera nor
-#  with an authorized and properly licensed third party, you do not
-#  have any rights to access nor to use this code.
-#
-#  Absent a written agreement with Cloudera, Inc. ("Cloudera") to the
-#  contrary, A) CLOUDERA PROVIDES THIS CODE TO YOU WITHOUT WARRANTIES OF ANY
-#  KIND; (B) CLOUDERA DISCLAIMS ANY AND ALL EXPRESS AND IMPLIED
-#  WARRANTIES WITH RESPECT TO THIS CODE, INCLUDING BUT NOT LIMITED TO
-#  IMPLIED WARRANTIES OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY AND
-#  FITNESS FOR A PARTICULAR PURPOSE; (C) CLOUDERA IS NOT LIABLE TO YOU,
-#  AND WILL NOT DEFEND, INDEMNIFY, NOR HOLD YOU HARMLESS FOR ANY CLAIMS
-#  ARISING FROM OR RELATED TO THE CODE; AND (D)WITH RESPECT TO YOUR EXERCISE
-#  OF ANY RIGHTS GRANTED TO YOU FOR THE CODE, CLOUDERA IS NOT LIABLE FOR ANY
-#  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR
-#  CONSEQUENTIAL DAMAGES INCLUDING, BUT NOT LIMITED TO, DAMAGES
-#  RELATED TO LOST REVENUE, LOST PROFITS, LOSS OF INCOME, LOSS OF
-#  BUSINESS ADVANTAGE OR UNAVAILABILITY, OR LOSS OR CORRUPTION OF
-#  DATA.
-#
 import os
 import sys
 import time
@@ -73,18 +43,19 @@ import pandas as pd
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+from app.services.query.flexible_retriever import FlexibleRetriever
+from app.services.query.query_configuration import QueryConfiguration
+from app.services.query.simple_reranker import cohere_rerank
+
 from llama_index.core import VectorStoreIndex
-from llama_index.core.indices.vector_store import VectorIndexRetriever
 from llama_index.core.response_synthesizers import get_response_synthesizer
 from llama_index.core.query_engine import RetrieverQueryEngine
 
 from app.ai.vector_stores.qdrant import QdrantVectorStore
 from app.rag_types import RagPredictConfiguration
 from app.services import models
-from app.services.query import querier
 from app.services.query.querier import CUSTOM_PROMPT
 from app.services.query.chat_engine import FlexibleChatEngine
-
 
 # usage: uv run --env-file=../.env performance_testing/performance_testing.py <data_source_id> questions_mini.csv
 def main():
@@ -102,7 +73,8 @@ def main():
                 score_count = 0
                 max_score = 0
                 for question in questions:
-                    chat_engine = setup(use_question_condensing=condensing, question=question, use_hyde=hyde, data_source_id=data_source_id)
+                    chat_engine = setup(use_question_condensing=condensing, question=question, use_hyde=hyde,
+                                        data_source_id=data_source_id)
                     nodes = chat_engine.retrieve(message=question, chat_history=None)
                     if nodes:
                         max_score = max(max_score, max(node.score for node in nodes))
@@ -111,7 +83,8 @@ def main():
                         score_count += 1
                         for index, node in enumerate(nodes):
                             # timestamp,hyde,score,chunk_no,question
-                            details.write(f'{time.time()},{hyde},{node.score},{node.metadata.get("file_name")},{node.node_id}{index + 1},"{question}"\n')
+                            details.write(
+                                f'{time.time()},{hyde},{node.score},{node.metadata.get("file_name")},{node.node_id}{index + 1},"{question}"\n')
                     details.flush()
 
                 average_score = score_sum / score_count
@@ -122,11 +95,9 @@ def main():
                     f.flush()
 
 
-def setup(data_source_id: int, question: str, use_question_condensing=True, use_hyde=True):
-    configuration = RagPredictConfiguration(use_question_condensing=use_question_condensing, top_k=5,
-                                            model_name="meta.llama3-1-8b-instruct-v1:0",
-                                            use_hyde=use_hyde)
-    llm = models.get_llm(model_name=configuration.model_name)
+def setup(data_source_id: int, question: str, use_question_condensing=True, use_hyde=True) -> FlexibleChatEngine:
+    configuration = RagPredictConfiguration(use_question_condensing=use_question_condensing, use_hyde=use_hyde)
+    llm = models.get_llm(model_name="meta.llama3-1-8b-instruct-v1:0")
     response_synthesizer = get_response_synthesizer(llm=llm)
     qdrant_store = QdrantVectorStore.for_chunks(data_source_id)
     vector_store = qdrant_store.llama_vector_store()
@@ -135,15 +106,16 @@ def setup(data_source_id: int, question: str, use_question_condensing=True, use_
         vector_store=vector_store,
         embed_model=embedding_model,
     )
-    doc_ids = querier.filter_doc_ids_by_summary(data_source_id, embedding_model, llm, question)
-    retriever = VectorIndexRetriever(
+    retriever = FlexibleRetriever(
+        configuration=QueryConfiguration(top_k=5, model_name="meta.llama3-1-8b-instruct-v1:0",
+                                         use_question_condensing=use_question_condensing, use_hyde=use_hyde),
         index=index,
-        similarity_top_k=configuration.top_k,
-        embed_model=embedding_model,  # is this needed, really, if it's in the index?
-        doc_ids=doc_ids or None,
+        embedding_model=embedding_model,  # is this needed, really, if it's in the index?
+        data_source_id=data_source_id,
+        llm=llm,
     )
     query_engine = RetrieverQueryEngine(
-        retriever=retriever, response_synthesizer=response_synthesizer
+        retriever=retriever, response_synthesizer=response_synthesizer, node_postprocessors=[cohere_rerank()]
     )
     chat_engine = FlexibleChatEngine.from_defaults(query_engine=query_engine, llm=llm,
                                                    condense_question_prompt=CUSTOM_PROMPT)
