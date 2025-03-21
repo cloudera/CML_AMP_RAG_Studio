30a6924ff7da2f7061357be39d773f240d22587f
jwatson <jkwatson@gmail.com>
2025-03-21 10:36:07 -0700

    "work on project card layout"

--- ui/src/pages/Projects/ProjectsPage/index.tsx
+++ ui/src/pages/Projects/ProjectsPage/index.tsx
@@ -1,22 +1,18 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { Card } from "antd";
+import { Card, Flex, Typography } from "antd";
 import { getProjectsQueryOptions, Project } from "src/api/projectsApi";
+import { format } from "date-fns";
 
 const ProjectCard = ({ project }: { project: Project }) => {
   return (
-    <Card title={project.name}>
-      <p>{project.createdById}</p>
-      <p>
-        {new Date(project.timeCreated * 1000).toLocaleDateString("en-US", {
-          month: "long",
-          day: "numeric",
-          year: "numeric",
-          hour: "numeric",
-          minute: "numeric",
-          second: "numeric",
-          timeZone: "UTC",
-        })}
-      </p>
+    <Card title={project.name} style={{ width: 300 }}>
+      <Flex vertical>
+        <Typography.Text>Creator: {project.createdById}</Typography.Text>
+        <Typography.Title level={5}> Created on:</Typography.Title>
+        <Typography.Text>
+          {format(project.timeCreated * 1000, "MMM dd yyyy, pp")}
+        </Typography.Text>
+      </Flex>
     </Card>
   );
 };
@@ -24,11 +20,18 @@ const ProjectCard = ({ project }: { project: Project }) => {
 const ProjectsPage = () => {
   const projects = useSuspenseQuery(getProjectsQueryOptions);
   return (
-    <div>
-      {projects.data.map((project) => {
-        return <ProjectCard project={project} />;
-      })}
-    </div>
+    <Flex
+      vertical
+      align="end"
+      style={{ width: "80%", maxWidth: 1000, marginTop: 40 }}
+      gap={20}
+    >
+      <Flex gap={24}>
+        {projects.data.map((project) => {
+          return <ProjectCard project={project} />;
+        })}
+      </Flex>
+    </Flex>
   );
 };
 
