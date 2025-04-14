4f0fc82fcee69bc52bd69f1c91c4f3d0fd648152
Elijah Williams <ewilliams@cloudera.com>
2025-04-14 12:30:07 -0600

    create a qdrant app on refresh if not present

--- scripts/refresh_project.py
+++ scripts/refresh_project.py
@@ -38,6 +38,7 @@
 
 import os
 import subprocess
+import cmlapi
 
 root_dir = (
     "/home/cdsw/rag-studio" if os.getenv("IS_COMPOSABLE", "") != "" else "/home/cdsw"
@@ -47,6 +48,40 @@ os.chdir(root_dir)
 print(subprocess.run(["git", "stash"], check=True))
 print(subprocess.run(["git", "pull", "--rebase"], check=True))
 print(subprocess.run(["bash", "scripts/refresh_project.sh"], check=True))
+
+
+client = cmlapi.default_client()
+project_id = os.environ["CDSW_PROJECT_ID"]
+apps = client.list_applications(project_id=project_id)
+if len(apps.applications) > 0:
+    # find the application named "RagStudio" and restart it
+    ragstudio_qdrant = next(
+        (app for app in apps.applications if app.name == "RagStudioQdrant"), None
+    )
+    if ragstudio_qdrant:
+        if ragstudio_qdrant.status != "APPLICATION_RUNNING":
+            app_id = ragstudio_qdrant.id
+            print("Restarting app with ID: ", app_id)
+            client.restart_application(application_id=app_id, project_id=project_id)
+    else:
+        application = client.create_application(
+            project_id=project_id,
+            body={
+                "name": "RagStudioQdrant",
+                "subdomain": "ragstudioqdrant",
+                "bypass_authentication": False,
+                "static_subdomain": False,
+                "script": "scripts/startup_qdrant.py",
+                "short_summary": "Create and start RagStudio's Qdrant instance.",
+                "long_summary": "Create and start RagStudio Qdrant instance.",
+                "cpu": 2,
+                "memory": 4,
+                "environment_variables": {
+                    "TASK_TYPE": "START_APPLICATION",
+                },
+            },
+        )
+        client.restart_application(application_id=application.id, project_id=project_id)
 
 print(
     "Project refresh complete. Restarting the RagStudio Application to pick up changes, if this isn't the initial deployment."
