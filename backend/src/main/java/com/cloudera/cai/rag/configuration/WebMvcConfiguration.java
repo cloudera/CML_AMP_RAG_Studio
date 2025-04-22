c9cafe1a94ec305485b6fff4454d6736968b594c
jwatson <jkwatson@gmail.com>
2025-04-22 10:27:32 -0700

    add cors configurer

--- backend/src/main/java/com/cloudera/cai/rag/configuration/WebMvcConfiguration.java
+++ backend/src/main/java/com/cloudera/cai/rag/configuration/WebMvcConfiguration.java
@@ -50,6 +50,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -87,6 +88,21 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
       @Override
       public void afterCompletion(
           HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {}
+    };
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/*")
+            .allowedOrigins("*")
+            .allowCredentials(true)
+            .allowedMethods("*")
+            .allowedHeaders("*");
+      }
     };
   }
 
