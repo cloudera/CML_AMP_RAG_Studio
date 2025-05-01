7dc6282fe473a246dd63532b6de682ec4fd27bba
Elijah Williams <ewilliams@cloudera.com>
2025-05-01 14:25:25 -0700

    add tool calling option to the session config page

--- ui/src/pages/RagChatTab/SessionsSidebar/CreateSession/CreateSessionForm.tsx
+++ ui/src/pages/RagChatTab/SessionsSidebar/CreateSession/CreateSessionForm.tsx
@@ -76,6 +76,25 @@ const CreateSessionForm = ({ form, dataSources }: CreateSessionFormProps) => {
       children: (
         <>
           <Form.Item<CreateSessionType>
+            name={["queryConfiguration", "enableToolCalling"]}
+            initialValue={false}
+            valuePropName="checked"
+            label={
+              <Popover
+                title="Tool Calling"
+                content={
+                  <Typography style={{ width: 300 }}>
+                    Enables tool calling.
+                  </Typography>
+                }
+              >
+                Enable Tool Calling
+              </Popover>
+            }
+          >
+            <Switch />
+          </Form.Item>
+          <Form.Item<CreateSessionType>
             name={["queryConfiguration", "enableHyde"]}
             initialValue={false}
             valuePropName="checked"
