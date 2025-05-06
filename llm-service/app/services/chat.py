c89bebdff057bc389c56eb0b7c27a5a413d16640
Michael Liu <mliu@cloudera.com>
2025-05-06 14:01:53 -0700

    drop databases
    lastFile:llm-service/app/services/chat.py

--- llm-service/app/services/chat.py
+++ llm-service/app/services/chat.py
@@ -161,6 +161,7 @@ def _run_streaming_chat(
     )
     for response in streaming_chat_response.chat_stream:
         response.additional_kwargs["response_id"] = response_id
+        print(f"{response=}")
         yield response
 
     chat_response = AgentChatResponse(
@@ -168,6 +169,7 @@ def _run_streaming_chat(
         sources=streaming_chat_response.sources,
         source_nodes=streaming_chat_response.source_nodes,
     )
+    print(f"{chat_response=}")
 
     if condensed_question and (condensed_question.strip() == query.strip()):
         condensed_question = None
