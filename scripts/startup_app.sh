07f969afd15b6aba3d28521784507560e0e4d9ff
jwatson <jkwatson@gmail.com>
2025-06-11 15:31:39 -0700

    kill the startup script if python fails to start

--- scripts/startup_app.sh
+++ scripts/startup_app.sh
@@ -77,10 +77,15 @@ source scripts/load_nvm.sh > /dev/null
 # start Python backend
 cd llm-service
 mkdir -p $MLFLOW_RECONCILER_DATA_PATH
-uv run fastapi run --reload --host 127.0.0.1 --port 8081 2>&1 &
 
+uv run fastapi run --host 127.0.0.1 --port 8081 2>&1 &
+PY_BACKGROUND_PID=$!
 # wait for the python backend to be ready
 while ! curl --output /dev/null --silent --fail http://localhost:8081/amp; do
+    if ! kill -0 $PY_BACKGROUND_PID 2>/dev/null; then
+        echo "Python backend process exited unexpectedly."
+        exit 1
+    fi
     echo "Waiting for the Python backend to be ready..."
     sleep 4
 done
