f299425b9b98306d8be824132e1e51809a7f65a2
Elijah Williams <ewilliams@cloudera.com>
2025-07-07 16:54:15 -0600

    remove drop code from input
    lastFile:ui/src/pages/RagChatTab/FooterComponents/RagChatQueryInput.tsx

--- ui/src/pages/RagChatTab/FooterComponents/RagChatQueryInput.tsx
+++ ui/src/pages/RagChatTab/FooterComponents/RagChatQueryInput.tsx
@@ -36,7 +36,7 @@
  * DATA.
  ******************************************************************************/
 
-import { Button, Flex, Input, InputRef, Tooltip, Upload } from "antd";
+import { Button, Flex, Input, InputRef, Tooltip } from "antd";
 import {
   DatabaseFilled,
   DatabaseOutlined,
@@ -90,7 +90,6 @@ const RagChatQueryInput = ({
     strict: false,
   });
   const inputRef = useRef<InputRef>(null);
-  const [dragging, setDragging] = useState(false);
   const {
     data: sampleQuestions,
     isFetching: sampleQuestionsIsFetching,
@@ -168,14 +167,7 @@ const RagChatQueryInput = ({
   };
 
   return (
-    <div
-      onDragEnter={() => {
-        setDragging(true);
-      }}
-      onDragLeave={() => {
-        setDragging(false);
-      }}
-    >
+    <div>
       <Flex vertical align="center" gap={10}>
         {flatChatHistory.length > 0 ? (
           <SuggestedQuestionsFooter
@@ -192,36 +184,30 @@ const RagChatQueryInput = ({
         ) : null}
         <Flex style={{ width: "100%" }} justify="space-between" gap={5}>
           <div style={{ position: "relative", width: "100%" }}>
-            {dragging ? (
-              <Upload.Dragger>
-                <div />
-              </Upload.Dragger>
-            ) : (
-              <TextArea
-                ref={inputRef}
-                placeholder={
-                  dataSourceSize && dataSourceSize > 0
-                    ? "Ask a question"
-                    : "Chat with the LLM"
+            <TextArea
+              ref={inputRef}
+              placeholder={
+                dataSourceSize && dataSourceSize > 0
+                  ? "Ask a question"
+                  : "Chat with the LLM"
+              }
+              status={dataSourcesStatus === "error" ? "error" : undefined}
+              value={userInput}
+              onChange={(e) => {
+                setUserInput(e.target.value);
+              }}
+              onKeyDown={(e) => {
+                if (e.shiftKey && e.key === "Enter") {
+                  return null;
+                } else if (e.key === "Enter") {
+                  e.preventDefault();
+                  handleChat(userInput);
                 }
-                status={dataSourcesStatus === "error" ? "error" : undefined}
-                value={userInput}
-                onChange={(e) => {
-                  setUserInput(e.target.value);
-                }}
-                onKeyDown={(e) => {
-                  if (e.shiftKey && e.key === "Enter") {
-                    return null;
-                  } else if (e.key === "Enter") {
-                    e.preventDefault();
-                    handleChat(userInput);
-                  }
-                }}
-                autoSize={{ minRows: 2, maxRows: 20 }}
-                disabled={streamChatMutation.isPending}
-                style={{ paddingRight: 110 }}
-              />
-            )}
+              }}
+              autoSize={{ minRows: 2, maxRows: 20 }}
+              disabled={streamChatMutation.isPending}
+              style={{ paddingRight: 110 }}
+            />
             <div
               style={{
                 position: "absolute",
