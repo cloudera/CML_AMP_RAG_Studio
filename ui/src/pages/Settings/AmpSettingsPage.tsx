ffc56c709eb46abbe3bae9f006f59c8509b901a9
jwatson <jkwatson@gmail.com>
2025-07-18 14:06:37 -0700

    wip on formatting warnings
    lastFile:ui/src/pages/Settings/AmpSettingsPage.tsx

--- ui/src/pages/Settings/AmpSettingsPage.tsx
+++ ui/src/pages/Settings/AmpSettingsPage.tsx
@@ -103,58 +103,52 @@ const AmpSettingsPage = () => {
         !projectConfig.is_valid_config &&
         !confirmationModal.isModalOpen && (
           <>
-            <Alert
-              message={
-                <div>
-                  {projectConfig.config_validation_results.storage
-                    .valid ? null : (
+            {!projectConfig.config_validation_results.storage.valid ? (
+              <Alert
+                message={
+                  <div>
                     <Typography.Text>
-                      `Storage configuration is invalid: $
-                      {projectConfig.config_validation_results.storage.message}`
+                      Storage configuration is invalid:
+                      {projectConfig.config_validation_results.storage.message}
                     </Typography.Text>
-                  )}
-                </div>
-              }
-              type="warning"
-              showIcon
-              style={{ marginTop: 40, width: "fit-content" }}
-            />
-            <Alert
-              message={
-                <div>
-                  {projectConfig.config_validation_results.model
-                    .valid ? null : (
+                  </div>
+                }
+                type="warning"
+                showIcon
+                style={{ marginTop: 40, width: "fit-content" }}
+              />
+            ) : null}
+            {!projectConfig.config_validation_results.model.valid ? (
+              <Alert
+                message={
+                  <div>
                     <Typography.Text>
-                      `Model configuration is invalid: $
-                      {projectConfig.config_validation_results.model.message}`
+                      Model configuration is invalid:
+                      {projectConfig.config_validation_results.model.message}
                     </Typography.Text>
-                  )}
-                </div>
-              }
-              type="warning"
-              showIcon
-              style={{ marginTop: 40, width: "fit-content" }}
-            />
-            <Alert
-              message={
-                <div>
-                  {projectConfig.config_validation_results.metadata_api
-                    .valid ? null : (
-                    <Typography.Text>
-                      `Metadata database configuration is invalid: $
-                      {
-                        projectConfig.config_validation_results.metadata_api
-                          .message
-                      }
-                      `
-                    </Typography.Text>
-                  )}
-                </div>
-              }
-              type="warning"
-              showIcon
-              style={{ marginTop: 40, width: "fit-content" }}
-            />
+                  </div>
+                }
+                type="warning"
+                showIcon
+                style={{ marginTop: 40, width: "fit-content" }}
+              />
+            ) : null}
+            {!projectConfig.config_validation_results.metadata_api.valid ? (
+              <Alert
+                message="Metadata database configuration is invalid"
+                description={
+                  <Typography.Text type={"secondary"}>
+                    {
+                      projectConfig.config_validation_results.metadata_api
+                        .message
+                    }
+                  </Typography.Text>
+                }
+                type="warning"
+                showIcon
+                style={{ marginTop: 40, width: "fit-content" }}
+              />
+            ) : null}
           </>
         )}
       {showWarning && (
