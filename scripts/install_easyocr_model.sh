693a80fab066d8ef585ff26149d1d08c2412aa3e
Baasit Sharief <baasitsharief@gmail.com>
2025-10-17 12:27:36 -0700

    fix: simplify EasyOCR model installation script by removing existing file check

--- scripts/install_easyocr_model.sh
+++ scripts/install_easyocr_model.sh
@@ -38,16 +38,11 @@
 # DATA.
 #
 
-CRAFT_MLT_MODEL=/app/prebuilt_artifacts/models/craft_mlt_25k.pth
-LATIN_G2_MODEL=/app/prebuilt_artifacts/models/latin_g2.pth
+CRAFT_MLT_MODEL=prebuilt_artifacts/models/craft_mlt_25k.pth
+LATIN_G2_MODEL=prebuilt_artifacts/models/latin_g2.pth
 
 mkdir -p .EasyOCR/model
 cd .EasyOCR/model
 
-# If model files exist in the current directory then don't copy them from prebuilt_artifacts
-if [ -f craft_mlt_25k.pth ] && [ -f latin_g2.pth ]; then
-    echo "Model files already exist in the current directory"
-else
-    cp ${CRAFT_MLT_MODEL} craft_mlt_25k.pth
-    cp ${LATIN_G2_MODEL} latin_g2.pth
-fi
+cp ${CRAFT_MLT_MODEL} craft_mlt_25k.pth
+cp ${LATIN_G2_MODEL} latin_g2.pth
